05694f617f817f0c48375b1dd348a0e6e0fd4ac8
lnwpor <tclnwpor@gmail.com>
2015-11-24 17:04:07 +0000

    edit route for user

--- lnwToDoApp-server-nodejs/routes/api.js
+++ lnwToDoApp-server-nodejs/routes/api.js
@@ -4,51 +4,56 @@ var express = require('express'),
     User = require('../models/user'),
     List = require('../models/list')
     
-router.post('/users/register', function(req, res) {
-  User.register(new User({ username: req.body.username }), req.body.password, function(err, account) {
-    if (err) {
-      return res.status(500).json({err: err})
-    }
-    passport.authenticate('local')(req, res, function () {
-      return res.status(200).json({status: 'Registration successful!'})
+router.route('/users/register') 
+  .post(function(req, res) {
+    User.register(new User({ username: req.body.username }), req.body.password, function(err, account) {
+      if (err) {
+        return res.status(500).json({err: err})
+      }
+      passport.authenticate('local')(req, res, function () {
+        return res.status(200).json({status: 'Registration successful!'})
+      });
     });
   });
-});
 
-router.post('/users/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) { return next(err) }
-    if (!user) {
-      return res.status(401).json({err: info})
-    }
-    req.logIn(user, function(err) {
-      if (err) {
-        return res.status(500).json({err: 'Could not log in user'})
+router.route('/users/login') 
+  .post(function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+      if (err) { return next(err) }
+      if (!user) {
+        return res.status(401).json({err: info})
       }
-      res.status(200).json({status: 'Login successful!'})
-    });
-  })(req, res, next);
-});
+      req.logIn(user, function(err) {
+        if (err) {
+          return res.status(500).json({err: 'Could not log in user'})
+        }
+        res.status(200).json({status: 'Login successful!'})
+      });
+    })(req, res, next);
+  });
 
-router.get('/users/logout', function(req, res) {
-  req.logout();
-  res.status(200).json({status: 'Bye!'})
-});
+router.route('/users/logout')
+  .get( function(req, res) {
+    req.logout();
+    res.status(200).json({status: 'Bye!'})
+  });
 
-router.get('/users', function(req, res) {
+router.route('/users')
+  .get( function(req, res) {
     User.find(function(err, users ) {
-        if (err)
-            res.send(err)
-        res.json(users);
+      if (err)
+        res.send(err)
+      res.json(users);
     });
-});
+  });
 
-router.get('/users/:id', function(req, res) {
+router.route('/users/:id') 
+  .get( function(req, res) {
     User.findOne({ _id: req.params.id }, function(err, user) {
       if (err) return console.error(err);
         res.json(user);
     });
-});
+  });
 
 router.route('/lists')
 
